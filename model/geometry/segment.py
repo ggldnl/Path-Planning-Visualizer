e58f2adb9a05251fa38e106e6305bffa4633daeb
Re-l <danielgigliotti99.dg@gmail.com>
2023-11-14 09:58:11 +0100

    added getitem and setitem methods to Segment class

--- model/geometry/segment.py
+++ model/geometry/segment.py
@@ -7,6 +7,25 @@ class Segment:
         self.start = start
         self.end = end
 
+    def __getitem__(self, item):
+        if item == 0:
+            return self.start
+        elif item == 1:
+            return self.end
+        raise IndexError(f'Segment index out of range: {item}')
+
+    def __setitem__(self, key, value):
+
+        if not isinstance(value, Point):
+            key_name = 'start' if key == 0 else 'end'
+            raise ValueError(f'Invalid {key_name} object {type(value)}; must be Point.')
+
+        if key == 0:
+            self.start = value
+        elif key == 1:
+            self.end = value
+        raise IndexError(f'Segment index out of range: {key}')
+
     def _normal(self):
         # Compute the normal vector of the segment
         return Point(-(self.end.y - self.start.y), self.end.x - self.start.x)
