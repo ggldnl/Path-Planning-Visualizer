fdfab7b76ae4f551bc53df44fd1220b4cedcdc87
Re-l <danielgigliotti99.dg@gmail.com>
2023-11-26 21:47:23 +0100

    moved obstacle control in initialization to support repeated random map

--- model/world/controllers/a_r_controller.py
+++ model/world/controllers/a_r_controller.py
@@ -26,9 +26,6 @@ class AStarController(Controller):
 
         super().__init__(robot, map, iterations)
 
-        # Disable moving obstacles for the map
-        self.map.disable_moving_obstacles()
-
         self.start = None
 
         self.open_set = []  # Nodes to be explored
@@ -48,6 +45,9 @@ class AStarController(Controller):
         self.initialize()
 
     def initialize(self):
+
+        # Disable moving obstacles for the map
+        self.map.disable_moving_obstacles()
 
         self.start = self.robot.current_pose.as_point()
 
