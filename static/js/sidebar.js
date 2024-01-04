7538c9dd5ad4ca32f8029f4c3f8b345520ed1489
daniel <danielgigliotti99.dg@gmail.com>
2024-01-05 00:50:58 +0100

    added second sidebar (algorithm selection)

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,19 +1,36 @@
-const sidebar = document.getElementById("sidebar");
-const open_btn = document.getElementById('open-btn');
+const left_sidebar = document.getElementById("left-sidebar");
+const left_open_btn = document.getElementById('left-open-btn');
 const home_btn = document.getElementById('home-btn');
 
-open_btn.onclick = function (event) {
+left_open_btn.onclick = function (event) {
 
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-        open_btn.classList.remove('shifted');
+    if (left_sidebar.style.width === '250px') {
+        left_sidebar.style.width = '0';
+        left_open_btn.classList.remove('shifted');
         home_btn.classList.remove('shifted');
-        open_btn.innerHTML = '<i class="fas fa-bars"></i>';
+        left_open_btn.innerHTML = '<i class="fas fa-bars"></i>';
     } else {
-        sidebar.style.width = '250px';
-        open_btn.classList.add('shifted');
+        left_sidebar.style.width = '250px';
+        left_open_btn.classList.add('shifted');
         home_btn.classList.add('shifted');
-        open_btn.innerHTML = '<i class="fas fa-times"></i>';;
+        left_open_btn.innerHTML = '<i class="fas fa-times"></i>';;
+    }
+
+}
+
+const right_sidebar = document.getElementById("right-sidebar");
+const right_open_btn = document.getElementById('right-open-btn');
+
+right_open_btn.onclick = function (event) {
+
+    if (right_sidebar.style.width === '250px') {
+        right_sidebar.style.width = '0';
+        right_open_btn.classList.remove('shifted');
+        right_open_btn.innerHTML = '<i class="fas fa-bars"></i>';
+    } else {
+        right_sidebar.style.width = '250px';
+        right_open_btn.classList.add('shifted');
+        right_open_btn.innerHTML = '<i class="fas fa-times"></i>';;
     }
 
 }
