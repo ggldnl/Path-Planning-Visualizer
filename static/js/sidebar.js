e31473c89bf1b6da369fd7842ed64a6950d733f4
daniel <danielgigliotti99.dg@gmail.com>
2024-01-06 16:13:17 +0100

    updated right sidebar icon

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -26,7 +26,7 @@ right_open_btn.onclick = function (event) {
     if (right_sidebar.style.width === '250px') {
         right_sidebar.style.width = '0';
         right_open_btn.classList.remove('shifted');
-        right_open_btn.innerHTML = '<i class="fas fa-bars"></i>';
+        right_open_btn.innerHTML = '<i class="fas fa-gamepad"></i>';
     } else {
         right_sidebar.style.width = '250px';
         right_open_btn.classList.add('shifted');
