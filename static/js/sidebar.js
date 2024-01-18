c16b66817858fb1b2292f8b50d0108ab02a84ac1
daniel <danielgigliotti99.dg@gmail.com>
2024-01-18 21:20:26 +0100

    now sidebars slide with the same width

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -9,14 +9,14 @@ const track_btn = document.getElementById('track-btn');
 
 left_open_btn.onclick = function (event) {
 
-    if (left_sidebar.style.width === '250px') {
-        left_sidebar.style.width = '0';
+    if (left_sidebar.classList.contains('shifted')) {
+        left_sidebar.classList.remove('shifted');
         left_open_btn.classList.remove('shifted');
         home_btn.classList.remove('shifted');
         track_btn.classList.remove('shifted');
         left_open_btn.innerHTML = '<i class="fas fa-bars"></i>';
     } else {
-        left_sidebar.style.width = '250px';
+        left_sidebar.classList.add('shifted');
         left_open_btn.classList.add('shifted');
         home_btn.classList.add('shifted');
         track_btn.classList.add('shifted');
@@ -30,12 +30,12 @@ const right_open_btn = document.getElementById('right-open-btn');
 
 right_open_btn.onclick = function (event) {
 
-    if (right_sidebar.style.width === '250px') {
-        right_sidebar.style.width = '0';
+    if (right_sidebar.classList.contains('shifted')) {
+        right_sidebar.classList.remove('shifted');
         right_open_btn.classList.remove('shifted');
         right_open_btn.innerHTML = '<i class="fas fa-gamepad"></i>';
     } else {
-        right_sidebar.style.width = '250px';
+        right_sidebar.classList.add('shifted');
         right_open_btn.classList.add('shifted');
         right_open_btn.innerHTML = '<i class="fas fa-times"></i>';;
     }
