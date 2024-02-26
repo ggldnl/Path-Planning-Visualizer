05e9eb361976db9db71595c4495a5a972aa49d0b
Iacopo Modica <iacopo.modica@gmail.com>
2024-02-26 23:34:14 +0100

    modified InformedRRTStar to improve ellipse handling and point generation

--- model/controllers/sampling_based/InformedRRTStar.py
+++ model/controllers/sampling_based/InformedRRTStar.py
@@ -2,6 +2,7 @@ import random
 
 from model.controllers.sampling_based_algorithm import SamplingBased
 from model.controllers.graph import Node
+from model.geometry.ellipse import Ellipse
 
 from model.geometry.segment import Segment
 from model.geometry.point import Point
@@ -26,6 +27,7 @@ class InformedRRTStar(SamplingBased):
         self.step_length = step_length
         self.search_radius = search_radius
         self.goal_sample_rate = goal_sample_rate
+        self.ellipse = None
 
     def heuristic(self, point):
         return 0
@@ -34,37 +36,72 @@ class InformedRRTStar(SamplingBased):
 
         self.nodes = [Node(self.start)]
         self.edges = []
-        self.current_iteration = 0
-
-        self.path = []
-        self.draw_list = []
-
-        self.map.disable_moving_obstacles()
-        self.planning_done = False
+        self.need_for_path = True
 
     def step_search(self):
-        self.current_iteration += 1
 
+        if self.need_for_path:
+            self.step_planning()
+        else:
+            self.step_replanning()
+
+    def step_planning(self):
         node_rand = self.generate_random_node()
         node_near = self.nearest_neighbor(node_rand)
         node_new = self.new_state(node_near, node_rand)
 
         if node_new and not self.check_collision(node_near.point, node_new.point):
-            nodes_near_index = self.find_neighborhood(node_new)
-            self.choose_parent(node_new, nodes_near_index)
-            self.rewire(node_new, nodes_near_index)
+            neighbor_index = self.find_neighborhood(node_new)
 
-            if self.distance_to_goal(node_new) < self.step_length:
-                if not self.check_collision(node_new, self.map.goal):
-                    self.nodes.append(node_new)
+            self.nodes.append(node_new)
 
-            self.update_draw_list(None)
+            if neighbor_index:
+                self.choose_parent(node_new, neighbor_index)
+                self.rewire(node_new, neighbor_index)
+
+            dist = self.distance_to_goal(node_new)
+
+            if dist <= self.step_length:
+                self.extract_path(node_new)
+
+            self.update_draw_list()
+
+    def step_replanning(self):
+        node_rand = self.generate_random_node_replanning()
+        node_near = self.nearest_neighbor(node_rand)
+        node_new = self.new_state(node_near, node_rand)
+
+        print(self.check_collision(node_near.point, node_new.point))
+        if node_new and not self.check_collision(node_near.point, node_new.point):
+            neighbor_index = self.find_neighborhood(node_new)
+
+            self.nodes.append(node_new)
+
+            if neighbor_index:
+                self.choose_parent(node_new, neighbor_index)
+                self.rewire(node_new, neighbor_index)
+
+            dist = self.distance_to_goal(node_new)
+
+            if dist <= self.step_length:
+                self.extract_path(node_new)
+
+            self.update_draw_list()
+
+    def generate_random_node_replanning(self):
+        if self.path_nodes:
+            if self.ellipse is None:
+                a1, b1 = self.path_nodes[0]
+                a2, b2 = self.world_map.goal.x, self.world_map.goal.y
+                c = Point(x=a1, y=b1).distance(Point(x=a2, y=b2))
+                self.ellipse = Ellipse.from_path_points(a1, b1, a2, b2, c)
+
+            random_node_inside_ellipse = Node(Point.from_dict(self.ellipse.generate_point_inside()))
+            return random_node_inside_ellipse
 
     def post_search(self):
-
-        index = self.search_goal_parent()
-        if index > 0:
-            self.extract_path(self.nodes[index])
+        if not self.need_for_path:
+            self.path = [node for node in self.path_nodes]
 
     def check_collision(self, point_start, point_end):
         if point_start == point_end:
@@ -77,23 +114,21 @@ class InformedRRTStar(SamplingBased):
 
         if len(node_index) > 0:
             cost_list = [dist_list[i] + self.compute_cost(self.nodes[i]) for i in node_index
-                         if not self.nodes[i].point == self.map.goal and
-                         not self.check_collision(self.nodes[i].point, self.map.goal)]
+                         if not self.nodes[i].point == self.world_map.goal and
+                         not self.check_collision(self.nodes[i].point, self.world_map.goal)]
             return node_index[int(np.argmin(cost_list))]
         return -1
 
     def find_neighborhood(self, node_new):
         dist_table = [np.hypot(nd.point.x - node_new.point.x, nd.point.y - node_new.point.y) for nd in self.nodes]
         dist_table_index = [ind for ind in range(len(dist_table)) if dist_table[ind] <= self.search_radius and
-                            not node_new.point == self.map.goal and
+                            not node_new.point == self.world_map.goal and
                             not self.check_collision(node_new.point, self.nodes[ind].point)]
 
         return dist_table_index
 
     def has_path(self):
-        return len(self.path) > 0 and self.path[-1] == self.map.goal
-
-
+        return len(self.path) > 0 and self.path[-1] == self.world_map.goal
 
     def nearest_neighbor(self, n):
         return min(self.nodes, key=lambda nd: nd.point.distance(n.point))
@@ -111,14 +146,15 @@ class InformedRRTStar(SamplingBased):
         return node_new
 
     def extract_path(self, node_end):
-        self.path = [Point(self.map.goal[0], self.map.goal[1])]
+        self.need_for_path = False
+        self.path_nodes = [Node(self.world_map.goal)]
         node_now = node_end
 
         while node_now.parent is not None:
             node_now = node_now.parent
-            self.path.append(Point(node_now.point.x, node_now.point.y))
+            self.path_nodes.append(Point(node_now.point.x, node_now.point.y))
 
-        self.path = self.path[::-1]
+        self.path_nodes = self.path_nodes[::-1]
 
     def choose_parent(self, node_new, neighbor_index):
         cost = [self.get_new_cost(self.nodes[i], node_new) for i in neighbor_index]
@@ -132,4 +168,33 @@ class InformedRRTStar(SamplingBased):
             if self.compute_cost(node_neighbor) > self.get_new_cost(node_new, node_neighbor):
                 node_neighbor.parent = node_new
 
+    def get_new_cost(self, node_start, node_end):
+        dist, _ = self.get_distance_and_angle(node_start, node_end)
+        return self.compute_cost(node_start) + dist
 
+    @staticmethod
+    def get_distance_and_angle(node_start, node_end):
+        dx = node_end.point.x - node_start.point.x
+        dy = node_end.point.y - node_start.point.y
+        return node_start.point.distance(node_end.point), np.arctan2(dy, dx)
+
+    @staticmethod
+    def compute_cost(node):
+        n = node
+        cost = 0.0
+        while n.parent:
+            cost += n.point.distance(n.parent.point)
+            n = n.parent
+        return cost
+
+    def generate_random_node(self):
+        if np.random.random() > self.goal_sample_rate:
+            x = np.random.uniform(-2 * self.world_map.obs_max_dist, 0) + self.world_map.obs_max_dist
+            y = np.random.uniform(-2 * self.world_map.obs_max_dist, 0) + self.world_map.obs_max_dist
+        else:
+            x, y = self.world_map.goal
+
+        return Node(Point(x, y))
+
+    def distance_to_goal(self, node):
+        return node.point.distance(self.world_map.goal)
